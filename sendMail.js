095ce95c661171ed3d640622a2f548ca23ccb91a
ToyVoCeyVo <ramp8a799@gmail.com>
2021-11-20 10:20:19 +0200

    Added validation internally

--- sendMail.js
+++ sendMail.js
@@ -1,25 +1,25 @@
-import {validateName,validateEmail,validateMessage,validateNumber} from "./Validation/validation";
+//import {validateName,validateEmail,validateMessage,validateNumber} from "./validation.js";
 
-let name = document.getElementById("name").value
-let email = document.getElementById("email").value
-let message = document.getElementById("message").value
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+let message = document.getElementById("message")
 let error = document.querySelector(".error-message")
 
 
 function validation(name,email,message) {
     while(true) {
-        if(!validateName(name)) {
+        if(!validateName(name.value)) {
             error.classList.remove('hide')
             error.textContent="Name is incorrect"
 
 
         }
-        else if(!validateEmail(email)) {
+        else if(!validateEmail(email.value)) {
             error.classList.remove('hide')
             error.textContent="Email is incorrect"
 
         }
-        else if(!validateMessage(message)) {
+        else if(!validateMessage(message.value)) {
             error.classList.remove('hide')
             error.textContent="Message is incorrect"
 
@@ -35,12 +35,31 @@ function validation(name,email,message) {
 function sendMail(params) {
     validation(name,email,message);
     let tempParams = {
-        from_name:name,
-        from_email:email,
-        message: message
+        from_name:name.value,
+        from_email:email.value,
+        message: message.value
     }
     emailjs.send('service_r6f0duy','template_3qvjzop',tempParams)
         .then((res)=> {
             console.log(res.status)
         })
+}
+
+ function validateName(name) {
+    return name.length > 3 && name.trim().length > 3
+}
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateMessage(message) {
+    return message.length > 10 && message.trim().length > 10
+}
+
+
+function validateNumber(number) {
+    return /^\d+$/.test(number) && number.length === 11;
 }
--- validation.js
+++ validation.js
@@ -1,21 +1,20 @@
-function validateName(name) {
+export function validateName(name) {
     return name.length > 3 && name.trim().length > 3
 }
-export {validateName}
 
-function validateEmail(email) {
+
+export function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
-export {validateEmail}
 
-function validateMessage(message) {
+export function validateMessage(message) {
     return message.length > 10 && message.trim().length > 10
 }
-export {validateMessage}
 
-function validateNumber(number) {
+
+export function validateNumber(number) {
     return /^\d+$/.test(number) && number.length === 11;
 }
-export {validateNumber}
+
 
