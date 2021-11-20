bbd616fd1d39d748460240775b498b9375207d6d
ToyVoCeyVo <ramp8a799@gmail.com>
2021-11-20 16:43:37 +0200

    Added spinner

--- Scripts/sendMail.js
+++ Scripts/sendMail.js
@@ -7,6 +7,8 @@ export let message = document.getElementById("message")
 export let error = document.querySelector(".error-message")
 export const submit = document.querySelector(".submit");
 
+const spinner = document.getElementById('spinner');
+
 NameValidation();
 EmailValidation();
 MessageValidation();
@@ -18,12 +20,16 @@ submit.addEventListener('click',(event)=> {
         from_email:email.value,
         message: message.value
     }
+
     emailjs.send('service_r6f0duy','template_3qvjzop',tempParams)
         .then(()=>{
+            spinner.classList.remove('hide');
+            setTimeout(()=>{spinner.classList.add('hide')},5*1000)
             console.log("Success");
             name.value='';
             email.value='';
             message.value='';},
             (error)=>{console.log("Error :",error)})
+
 })
 
