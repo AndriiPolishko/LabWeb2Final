c7d1edcc73bb39a08d16fdfa4e47778a8cf4e2bf
Andrii <ramp8a799@gmail.com>
2022-01-12 21:58:45 +0200

    Fixed validation

--- Scripts/Validation/validation.js
+++ Scripts/Validation/validation.js
@@ -1,61 +1,75 @@
-function validateName(name) {
-    return name.trim().length > 3
-}
 
+let error = document.querySelector("#error")
+import {name,email,message, submit,sendMail} from "../sendMail.js";
 
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+let form = document.querySelector(".contact-form")
+name.addEventListener('input',function(event) {
+   if(name.validity.valid) {
+       error.textContent="";
+       error.style.backgroundColor="var(--black)";
+   }
+   else {
+       showError();
+   }
+});
 
-import {error, name,email,message, submit} from "../sendMail.js";
-
-function mouseMove(part,validateFunc) {
-    if(!validateFunc(part.value)) {
-        block(`${part.id}`)
+email.addEventListener('input',function(event) {
+    if(email.validity.valid) {
+        error.textContent="";
+        error.style.backgroundColor="var(--black)";
     }
     else {
-        if(error.textContent !== `${part.id} is incorrect`)
-            submit.textContent = "Blocked"
-        else {
-            //error.classList.add('hide')
-            error.textContent=''
-            error.style.backgroundColor = "inherit"
-            submit.removeAttribute('disabled')
-            submit.textContent = "Send"
-        }
+        showError();
     }
-}
+});
 
-function val(part,validateFunc) {
+message.addEventListener('input',function(event) {
+    if(message.validity.valid) {
+        error.textContent="";
+        error.style.backgroundColor="var(--black)";
+    }
+    else {
+        showError();
+    }
+});
 
-    part.addEventListener('mouseleave',()=> {
-        mouseMove(part,validateFunc)
-    })
+form.addEventListener('submit', function(event) {
+    if(!name.validity.valid || !email.validity.valid || !message.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+    else {
+        sendMail(event)
+    }
+})
 
-    part.addEventListener('mouseenter',()=> {
-        mouseMove(part,validateFunc)
-    })
-
-}
-
-function block(part) {
-    error.classList.remove('hide')
-    error.textContent=`${part} is incorrect`
-    error.style.backgroundColor = "red"
-    submit.setAttribute('disabled','true')
-    submit.textContent = "Blocked"
-}
-
-export function NameValidation() {
-    val(name,validateName);
-}
-
-export function EmailValidation() {
-    val(email,validateEmail)
-}
-
-export function MessageValidation() {
-    val(message,validateName)
+function showError() {
+    if(name.validity.valueMissing) {
+        error.textContent = "Enter your name, please";
+        console.log("err1")
+    }
+    else if(name.validity.tooShort) {
+        error.textContent = "Your name is too short";
+        console.log("err2")
+    }
+    else if(email.validity.valueMissing) {
+        error.textContent = "Enter your Email, please";
+        console.log("err3")
+    }
+    else if(email.validity.typeMismatch) {
+        error.textContent = "Enter correct Email, please";
+        console.log("err4")
+    }
+    else if(email.validity.tooShort) {
+        error.textContent = "Your Email is too short";
+        console.log("err5")
+    }
+    else if(message.validity.valueMissing) {
+        error.textContent = "Enter your message, please";
+    }
+    else if(message.validity.tooShort) {
+        error.textContent = "Your message is too short";
+    }
+    error.style.backgroundColor="var(--red)"
 }
 
--- Scripts/sendMail.js
+++ Scripts/sendMail.js
@@ -1,4 +1,4 @@
-import {NameValidation,EmailValidation,MessageValidation} from "./Validation/validation.js";
+/*import {NameValidation,EmailValidation,MessageValidation} from "./Validation/validation.js";*/
 
 
 export let name = document.getElementById("name")
@@ -7,18 +7,12 @@ export let email = document.getElementById("email")
 
 export let message = document.getElementById("message")
 
-export let error = document.querySelector(".error-message")
-
 export const submit = document.querySelector(".submit");
 
 
 const spinner = document.getElementById('spinner');
 
-NameValidation();
-EmailValidation();
-MessageValidation();
-
-submit.addEventListener('click',(event)=> {
+export function sendMail(event) {
     submit.setAttribute('disabled','true')
     submit.textContent = "Blocked"
     event.preventDefault();
@@ -30,22 +24,21 @@ submit.addEventListener('click',(event)=> {
 
     emailjs.send('service_r6f0duy','template_3qvjzop',tempParams)
         .then(()=>{
-            spinner.classList.remove('hide');
-            setTimeout(()=> {
-                spinner.classList.add('hide');
-                submit.removeAttribute('disabled')
-            },5*1000)
-            console.log("Success");
-             const pop_up = document.querySelector(".error-message")
-             pop_up.style.backgroundColor = "green"
-             pop_up.innerText = "Great Success!!!"
-            name.value='';
-            email.value='';
-            message.value='';
-            setTimeout(()=>
-                    document.querySelector(".error-message").innerText="",
-                5*1000)},
+                spinner.classList.remove('hide');
+                setTimeout(()=> {
+                    spinner.classList.add('hide');
+                    submit.removeAttribute('disabled')
+                },5*1000)
+                console.log("Success");
+                const pop_up = document.querySelector(".error-message")
+                pop_up.style.backgroundColor = "green"
+                pop_up.innerText = "Great Success!!!"
+                name.value='';
+                email.value='';
+                message.value='';
+                setTimeout(()=>
+                        document.querySelector(".error-message").innerText="",
+                    5*1000)},
             (error)=>{console.log("Error :",error)})
-
-})
+}
 
